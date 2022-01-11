82032c584fd7340860bc8426235966b2c9eeb8ce
nikkolasg <nikkolasg@gmail.com>
2022-01-11 22:23:26 +0000

    cleaned up

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,8 @@ use ark_r1cs_std::pairing::PairingVar;
 use ark_r1cs_std::prelude::*;
 use ark_relations::r1cs::{ConstraintSynthesizer, ConstraintSystemRef, SynthesisError};
 use ark_std::marker::PhantomData;
-use ark_std::{vec::Vec, Zero};
+use ark_std::vec::Vec;
+use rayon::prelude::*;
 use std::ops::MulAssign;
 
 // Goals:
@@ -12,9 +13,9 @@ use std::ops::MulAssign;
 // 1. prove the multiplication a*G
 #[derive(Clone)]
 pub struct PolyCircuit<E: PairingEngine, P: PairingVar<E>> {
-    g: E::G1Projective,  // generator
-    p1: E::G1Projective, // public input
-    s1: E::Fr,           // private input
+    g: E::G1Projective,         // generator
+    pubs: Vec<E::G1Projective>, // public input
+    coeffs: Vec<E::Fr>,         // private input
     _pairing_gadget: PhantomData<P>,
 }
 
@@ -23,22 +24,20 @@ where
     E: PairingEngine,
     P: PairingVar<E>,
 {
-    pub fn new(s: E::Fr) -> Self {
+    pub fn new(s: Vec<E::Fr>) -> Self {
         let g = E::G1Projective::prime_subgroup_generator();
-        let mut p = g.clone();
-        p.mul_assign(s.clone());
+        let pubs = s
+            .par_iter()
+            .map(|s| {
+                let mut p = g.clone();
+                p.mul_assign(s.clone());
+                p
+            })
+            .collect::<Vec<_>>();
         Self {
             g: g,
-            p1: p,
-            s1: s,
-            _pairing_gadget: PhantomData,
-        }
-    }
-    pub fn empty() -> Self {
-        Self {
-            g: E::G1Projective::zero(),
-            p1: E::G1Projective::zero(),
-            s1: E::Fr::zero(),
+            pubs,
+            coeffs: s,
             _pairing_gadget: PhantomData,
         }
     }
@@ -50,21 +49,30 @@ where
     P: PairingVar<E>,
 {
     fn generate_constraints(self, cs: ConstraintSystemRef<E::Fq>) -> Result<(), SynthesisError> {
-        let g = P::G1Var::new_variable(
+        let g = P::G1Var::new_variable_omit_prime_order_check(
             ark_relations::ns!(cs, "generator"),
             || Ok(self.g),
-            AllocationMode::Witness,
+            AllocationMode::Input,
         )?;
-        let p1 = P::G1Var::new_variable(
-            ark_relations::ns!(cs, "generate_p1"),
-            || Ok(self.p1),
-            AllocationMode::Witness,
-        )?;
-
-        let s1_bit: Vec<bool> = BitIteratorLE::new(self.s1.into_repr().as_ref().to_vec()).collect();
-        let s1_bits = Vec::new_witness(ark_relations::ns!(cs, "s1_bits"), || Ok(s1_bit))?;
-        let exp_p1 = g.scalar_mul_le(s1_bits.iter())?;
-        p1.enforce_equal(&exp_p1)?;
+        let coeffs: Vec<(_, _)> = self
+            .pubs
+            .into_iter()
+            .zip(self.coeffs.iter())
+            .map(|(p, s)| {
+                let pvar = P::G1Var::new_variable_omit_prime_order_check(
+                    ark_relations::ns!(cs, "generate_p1"),
+                    || Ok(p),
+                    AllocationMode::Input,
+                )?;
+                let bits: Vec<bool> = BitIteratorLE::new(s.into_repr().as_ref().to_vec()).collect();
+                let s_bits = Vec::new_witness(ark_relations::ns!(cs, "s1_bits"), || Ok(bits))?;
+                Ok((pvar, s_bits))
+            })
+            .collect::<Result<Vec<_>, _>>()?;
+        for (p, s) in coeffs.iter() {
+            let exp = g.scalar_mul_le(s.iter())?;
+            p.enforce_equal(&exp)?;
+        }
         Ok(())
     }
 }
@@ -73,20 +81,30 @@ where
 mod tests {
     use super::*;
     use ark_bls12_377::{constraints::PairingVar as EV, Bls12_377 as E};
-    use ark_bw6_761::BW6_761 as P;
-    use ark_ec::ProjectiveCurve;
-    use ark_groth16::Groth16;
-    use ark_snark::{CircuitSpecificSetupSNARK, SNARK};
-    use ark_std::One;
+    //use ark_bw6_761::BW6_761 as P;
+    //use ark_ec::ProjectiveCurve;
+    //use ark_groth16::Groth16;
+    use ark_relations::r1cs::ConstraintSystem;
+    //use ark_snark::{CircuitSpecificSetupSNARK, SNARK};
     use ark_std::UniformRand;
 
     #[test]
     fn poly() {
         let mut rng = ark_std::test_rng();
-        let circuit = PolyCircuit::<E, EV>::new(<E as PairingEngine>::Fr::rand(&mut rng));
-        let (pk, vk) = Groth16::<P>::setup(PolyCircuit::<E, EV>::empty(), &mut rng).unwrap();
-        let proof = Groth16::prove(&pk, circuit, &mut rng).unwrap();
-        let valid_proof = Groth16::verify(&vk, &vec![], &proof).unwrap();
-        assert!(valid_proof);
+        let n = 500;
+        let secrets = (0..n)
+            .map(|_| <E as PairingEngine>::Fr::rand(&mut rng))
+            .collect::<Vec<_>>();
+        let circuit = PolyCircuit::<E, EV>::new(secrets.clone());
+        let cs = ConstraintSystem::<<E as PairingEngine>::Fq>::new_ref();
+        circuit.generate_constraints(cs.clone()).unwrap();
+        println!("Num constraints: {}", cs.num_constraints());
+        assert!(cs.is_satisfied().unwrap());
+        /*let circuit2 = PolyCircuit::<E, EV>::new(secrets.clone()); // why can't I clne :(*/
+        //let circuit3 = PolyCircuit::<E, EV>::new(secrets); // why can't I clne :(
+        //let (pk, vk) = Groth16::<P>::setup(circuit2, &mut rng).unwrap();
+        //let proof = Groth16::prove(&pk, circuit3, &mut rng).unwrap();
+        //let valid_proof = Groth16::verify(&vk, &vec![], &proof).unwrap();
+        /*assert!(valid_proof);*/
     }
 }
