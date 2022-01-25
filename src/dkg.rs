e04948d8be2f1c3fb20b0de864c82af024878632
nikkolasg <nikkolasg@gmail.com>
2022-01-25 14:10:04 +0000

    adding logging remote repo

--- src/dkg.rs
+++ src/dkg.rs
@@ -26,7 +26,6 @@ where
     IV: PairingVar<I>,
 {
     conf: DKGConfig<I>,
-    coeffs: Vec<I::Fr>,
     pub_inputs: Vec<I::Fr>,
     inner_proof: GrothProof<I>,
     _op: PhantomData<O>,
@@ -43,15 +42,18 @@ where
         conf: DKGConfig<I>,
         mut rng: &mut R,
     ) -> Result<Self, SynthesisError> {
+        // -2 because -1 for the first coefficient which is the secret and -1
+        // because threshold is the number of evaluation pairs you need to
+        // reconstruct the poly, so it's the degree of the polynomial +1
+        // Therefore we want the degree to be threshold -1
         let coeffs = std::iter::once(conf.secret)
-            .chain((0..conf.threshold - 1).map(|_| I::Fr::rand(&mut rng)))
+            .chain((0..conf.threshold - 2).map(|_| I::Fr::rand(&mut rng)))
             .collect::<Vec<_>>();
-        let pe = PolyEvaluator::<I::Fr>::new(coeffs.clone(), conf.ids.clone());
+        let pe = PolyEvaluator::<I::Fr>::new(coeffs, conf.ids.clone());
         let pub_inputs = pe.public_inputs();
         let proof = Groth16::<I>::prove(&conf.inner_pk, pe, &mut rng)?;
         Ok(Self {
             conf: conf,
-            coeffs: coeffs,
             inner_proof: proof,
             pub_inputs: pub_inputs,
             _op: PhantomData,
@@ -63,6 +65,14 @@ where
         self,
         cs: ConstraintSystemRef<O::Fr>,
     ) -> Result<(), SynthesisError> {
+        println!("verifying native proof");
+        assert!(ark_groth16::verify_proof(
+            &self.conf.inner_vk,
+            &self.inner_proof,
+            &self.pub_inputs
+        )
+        .unwrap());
+        println!("verifying native proof PASSED");
         // prepare inputs, proof and vk vars
         let inputs_var =
             <Groth16VerifierGadget<I, IV> as SNARKGadget<
@@ -75,13 +85,13 @@ where
                 <I as PairingEngine>::Fr,
                 <I as PairingEngine>::Fq,
                 Groth16<I>,
-            >>::ProofVar::new_input(ns!(cs, "inner proof"), || Ok(self.inner_proof))?;
+            >>::ProofVar::new_witness(ns!(cs, "inner proof"), || Ok(self.inner_proof))?;
         let vk_var = <Groth16VerifierGadget<I, IV> as SNARKGadget<
             <I as PairingEngine>::Fr,
             <I as PairingEngine>::Fq,
             Groth16<I>,
-        >>::ProcessedVerifyingKeyVar::new_input(
-            ns!(cs, "inner proof"), || Ok(self.conf.inner_vk)
+        >>::ProcessedVerifyingKeyVar::new_witness(
+            ns!(cs, "inner vk"), || Ok(self.conf.inner_vk)
         )?;
         Groth16VerifierGadget::<I, IV>::verify_with_processed_vk(&vk_var, &inputs_var, &proof_var)?
             .enforce_equal(&Boolean::constant(true))?;
@@ -106,36 +116,51 @@ mod tests {
     use ark_bw6_761::BW6_761 as O;
     use ark_ec::ProjectiveCurve;
     use ark_groth16::Groth16;
-    use ark_relations::r1cs::ConstraintSystem;
+    use ark_relations::r1cs::{ConstraintLayer, ConstraintSystem, TracingMode};
     use ark_snark::{CircuitSpecificSetupSNARK, SNARK};
     use ark_std::UniformRand;
     use ark_std::Zero;
+    use tracing_subscriber::layer::SubscriberExt;
 
     #[test]
     fn dkg() {
         let mut rng = ark_std::test_rng();
-        let degree = 3;
+        let threshold = 3;
+        let degree = threshold - 1;
         let n = degree * 2;
         let secret = Fr::rand(&mut rng);
         let ids = (0..n).map(|i| Fr::from((i + 1) as u32)).collect::<Vec<_>>();
         // create pk, vk for inner proof
-        let null_coeffs = (0..degree + 1).map(|_| Fr::zero()).collect::<Vec<_>>();
-        let pe = PolyEvaluator::new(null_coeffs, ids.clone());
-        let (pk, vk) = Groth16::<I>::setup(pe, &mut rng).unwrap();
+        let (pk, vk) = {
+            let coeffs = std::iter::once(secret.clone())
+                .chain((0..threshold - 2).map(|_| Fr::rand(&mut rng)))
+                .collect::<Vec<_>>();
+            let pe = PolyEvaluator::<Fr>::new(coeffs, ids.clone());
+            Groth16::setup(pe, &mut rng).unwrap()
+        };
         let pvk = Groth16::<I>::process_vk(&vk).unwrap();
 
         //
         let config = DKGConfig {
             secret: secret,
-            threshold: degree,
+            threshold: threshold,
             ids: ids,
             inner_pk: pk,
             inner_vk: pvk,
         };
         let circuit = DKGCircuit::<O, I, IV>::new(config, &mut rng).unwrap();
+
+        let mut layer = ConstraintLayer::default();
+        layer.mode = TracingMode::OnlyConstraints;
+        let subscriber = tracing_subscriber::Registry::default().with(layer);
+        let _guard = tracing::subscriber::set_default(subscriber);
         let cs = ConstraintSystem::<<O as PairingEngine>::Fr>::new_ref();
         circuit.generate_constraints(cs.clone()).unwrap();
         println!("Num constraints: {}", cs.num_constraints());
-        assert!(cs.is_satisfied().unwrap());
+        assert!(
+            cs.is_satisfied().unwrap(),
+            "Constraints not satisfied: {}",
+            cs.which_is_unsatisfied().unwrap().unwrap_or_default()
+        );
     }
 }
